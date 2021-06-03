78905ae14e2c7af82fcef275448a2bbb4bd03915
Rafael Luiz Rehn <rafaelluizrehn@gmail.com>
2021-06-03 15:24:07 -0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { PokemonListComponent } from './pokemon/pokemon-list/pokemon-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:'', redirectTo: '/home', pathMatch: 'full'},
+  { path: 'home', component: HomeComponent },
+  { path: 'pokemons', component: PokemonListComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
